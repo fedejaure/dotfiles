a2c684b09de991b54d9ba06257f04c1f55e3f4e1
federico jaureguialzo <fedejaure@gmail.com>
2023-09-12 22:00:25 +0200

    Add support files

---
+++ tasks.py
@@ -0,0 +1,81 @@
+"""Tasks for maintaining the project.
+
+Execute 'invoke --list' for guidance on using Invoke
+"""
+import platform
+from pathlib import Path
+from typing import Any, Optional
+
+from invoke import call, task
+from invoke.context import Context
+from invoke.runners import Result
+
+ROOT_DIR = Path(__file__).parent
+PYTHON_TARGETS = [
+    Path(__file__),
+]
+PYTHON_TARGETS_STR = " ".join([str(p) for p in PYTHON_TARGETS])
+
+
+def _run(c: Context, command: str, env: dict[str, Any] | None = None) -> Optional[Result]:
+    return c.run(command, pty=platform.system() != "Windows", env=env)
+
+
+@task()
+def install_hooks(c: Context) -> None:
+    """Install pre-commit hooks."""
+    _run(c, "poetry run pre-commit install")
+
+
+@task()
+def hooks(c: Context) -> None:
+    """Run pre-commit hooks."""
+    _run(c, "poetry run pre-commit run --all-files")
+
+
+@task(name="format", help={"check": "Checks if source is formatted without applying changes"})
+def format_(c: Context, check: bool = False) -> None:
+    """Format code."""
+    isort_options = ["--check-only", "--diff"] if check else []
+    _run(c, f"poetry run isort {' '.join(isort_options)} {PYTHON_TARGETS_STR}")
+    black_options = ["--diff", "--check"] if check else ["--quiet"]
+    _run(c, f"poetry run black {' '.join(black_options)} {PYTHON_TARGETS_STR}")
+
+
+@task()
+def ruff(c: Context) -> None:
+    """Run ruff."""
+    _run(c, f"poetry run ruff check {PYTHON_TARGETS_STR}")
+
+
+@task()
+def security(c: Context) -> None:
+    """Run security related checks."""
+    _run(
+        c,
+        "poetry export --with dev --format=requirements.txt --without-hashes | "
+        "poetry run safety check --stdin --full-report",
+    )
+
+
+@task(pre=[call(format_, check=True), ruff, security])
+def lint(c: Context) -> None:
+    """Run all linting."""
+
+
+@task()
+def mypy(c: Context) -> None:
+    """Run mypy."""
+    _run(c, f"poetry run mypy {PYTHON_TARGETS_STR}")
+
+
+@task(
+    help={
+        "part": "Part of the version to be bumped.",
+        "dry_run": "Don't write any files, just pretend. (default: False)",
+    },
+)
+def version(c: Context, part: str, dry_run: bool = False) -> None:
+    """Bump version."""
+    bump_options = ["--dry-run"] if dry_run else []
+    _run(c, f"poetry run bump2version {' '.join(bump_options)} {part}")
